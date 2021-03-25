1b679bd3a2749398ca5cb3466ec24fa8c9729574
unknown <ibmjavanh11@iiht.tech>
2021-03-25 17:24:35 +0530

    refactored code

--- src/main/java/com/ibm/demo/OrderController.java
+++ src/main/java/com/ibm/demo/OrderController.java
@@ -1,14 +1,21 @@
 package com.ibm.demo;
+
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ibm.demo.entity.Order;
@@ -16,34 +23,45 @@ import com.ibm.demo.service.OrderService;
 
 @RestController
 public class OrderController {
-	@Autowired //Spring automatically performs injection
+	@Autowired // Spring automatically performs injection
 	OrderService orderService;
+
 	@PostMapping("/order")
+	@ResponseStatus(code = HttpStatus.CREATED) // to return status code 201
 //	String createOrder(@RequestBody Order order) {
 	String createOrder(@RequestBody @Valid Order order, BindingResult bindingResult) {
-		if (bindingResult.hasErrors()) {
-			throw new IllegalArgumentException();
-			
-		}
-		System.out.println(order); //front-end
-		return orderService.createOrder(order);//calls for service layer
+		validateModel(bindingResult);
+		System.out.println(order); // front-end
+		return orderService.createOrder(order);// calls for service layer
 	}
 
 	@GetMapping("/order")
-	String getOrder(Order order) {
-		System.out.println(order);
-		return "order created";
+	List<Order> getOrders() {
+		return orderService.getOrders();
+	}
+
+	@GetMapping("/order/{id}")
+	Order getOrder(@PathVariable("id") int orderId) {
+		return orderService.getOrder(orderId);
+	}
+
+	private void validateModel(Errors bindingResult) {
+		if (bindingResult.hasErrors()) {
+			throw new IllegalArgumentException("Something went wrong. Please try later");
+		}
 	}
 
 	@PutMapping("/order/{id}")
-	String updateOrder(@PathVariable("id") int orderId) {
+	String updateOrder(@RequestBody @Valid Order order, BindingResult bindingResult, @PathVariable("id") int orderId) {
+		validateModel(bindingResult);
 		System.out.println(orderId);
-		return "order updated";
+		return orderService.updateOrder(orderId);
 	}
 
 	@DeleteMapping("/order/{id}")
-	String deleteOrder(@PathVariable("id") int orderId) {
+	String deleteOrder(@PathVariable("id") int orderId, HttpRequest httpRequest) {
+		System.out.println(httpRequest.getHeaders());
 		System.out.println(orderId);
-		return "order deleted";
+		return orderService.deleteOrder(orderId);
 	}
 }
--- src/main/java/com/ibm/demo/service/OrderService.java
+++ src/main/java/com/ibm/demo/service/OrderService.java
@@ -1,13 +1,39 @@
 package com.ibm.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.ibm.demo.entity.Order;
 
 @Service
 public class OrderService { //Spring Bean:An object having special annotations in spring
+
 	public String createOrder(Order order) {
 		return "Order created";
 	}
+	
+	public String getOrder() {
+		return "order1";
+	}
+
+	public String updateOrder(int orderId) {
+		return "order updated";
+	}
+
+	public String deleteOrder(int orderId) {
+		System.out.println("order deleted");;
+		return "order deleted";
+	}
+
+	public Order getOrder(int orderId) {
+		return new Order();
+	}
+
+	public List<Order> getOrders() {
+		return new ArrayList<Order>();
+	}
+	
 
 }
