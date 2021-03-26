84d39b0e5cb8cb752cecb7af8eee3cd4ff96f040
unknown <ibmjavanh11@iiht.tech>
2021-03-26 11:35:54 +0530

    implemented createOrder with MongoDB

--- src/main/java/com/ibm/demo/service/OrderService.java
+++ src/main/java/com/ibm/demo/service/OrderService.java
@@ -3,17 +3,26 @@ package com.ibm.demo.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ibm.demo.entity.Order;
+import com.ibm.demo.repo.OrderRepository;
 
 @Service
-public class OrderService { //Spring Bean:An object having special annotations in spring
+public class OrderService { // Spring Bean:An object having special annotations in spring
+	@Autowired
+	OrderRepository orderRepository;// defining the dependancy as tis class is dependent on repository
 
 	public String createOrder(Order order) {
-		return "Order created";
+		Order savedOrder = orderRepository.save(order);
+		return savedOrder.getId();
 	}
-	
+
+	public List<Order> getOrders() {
+		return new ArrayList<Order>();
+	}
+
 	public String getOrder() {
 		return "order1";
 	}
@@ -23,17 +32,13 @@ public class OrderService { //Spring Bean:An object having special annotations i
 	}
 
 	public String deleteOrder(int orderId) {
-		System.out.println("order deleted");;
+		System.out.println("order deleted");
+		;
 		return "order deleted";
 	}
 
 	public Order getOrder(int orderId) {
 		return new Order();
 	}
-
-	public List<Order> getOrders() {
-		return new ArrayList<Order>();
-	}
-	
 
 }
